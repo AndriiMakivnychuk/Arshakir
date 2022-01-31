6295c255ff30a9189d36965342d53e8bc5e4ac12
Andrii <makivnicuka2@gmail.com>
2022-01-31 18:47:02 +0100

    add burger menu

---
+++ app/js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    function burgerMenu(selector) {
+        let menu = $(selector);
+        let button = menu.find('.burger-menu__button');
+        let links = menu.find('.burger-menu__link');
+
+
+        button.on('click', (e) => {
+            e.preventDefault();
+            toggleMenu();
+        });
+
+        links.on('click', () => toggleMenu());
+
+        function toggleMenu() {
+            menu.toggleClass('burger-menu__active');
+        }
+    }
+
+    burgerMenu('.burger-menu');
+})
